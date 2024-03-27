9ffb427681b81d835c7e0f8333466b52ed0d3d32
Shine0111 <ranshine9@gmail.com>
2024-03-27 12:32:34 +0100

    create ountUpper from ascii

--- string.c
+++ string.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #define SIZE 10
 
+int countUpper(char str[])
+{
+    int i = 0;
+    int uppers = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] != '\0')
+        {
+            uppers++;
+        }
+
+        i++;
+    }
+
+    return uppers;
+}
+
 int main()
 {
     char greet[SIZE] = "hello";
@@ -18,4 +35,7 @@ int main()
     name[i] = '\0'; // Issue is if the name[] don't have a \n in it the loop will be infinite unless by chance it will encounter a \n in the garbage memory
 
     printf("%s %s!\n", greet, name);
+
+    // Count the number of upperCase character in the string
+    int uppers = countUpper(name); // When we pass string into a function, we don't need to pass the length
 }
